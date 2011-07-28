f16e564e94480c380f2936b34554550b42ce5aaf
Eduard Nicodei <eddnicodei@gmail.com>
2011-07-28 12:31:10 +0300

    Can now calculate production for specific planets. Fixed various bugs related to inexistent db entries.

--- src/ogame-bot.py
+++ src/ogame-bot.py
@@ -1,4 +1,11 @@
-#! /usr/bin/python
+#!/usr/bin/python
+
+'''
+Created on Jul 28, 2011
+
+@author: eduard
+'''
+
 import MySQLdb, re, datetime
 
 class planet:
@@ -44,7 +51,9 @@ class ogameDB:
 		planet_list = [ id for id, in bot.cursor.fetchall() ]
 		rentability = []
 		for id in planet_list:
-			self.cursor.execute('select date, metal, crystal, deuterium from report where planet_id={} order by date desc;'.format(id))
+			if self.cursor.execute('select date, metal, crystal, deuterium from report where planet_id={} order by date desc;'.format(id)) == 0:
+				print 'No report for planet {}. Skipping...'.format(id)
+				continue
 			row_report = [ i for i in self.cursor.fetchone() ]
 			self.cursor.execute('select m_prod, c_prod, d_prod, coord, home_coord from planet where planet_id={}'.format(id))
 			prod = [ i for i in self.cursor.fetchone() ]
@@ -55,7 +64,6 @@ class ogameDB:
 			rentability.append((prod[3], round(float(current_res) / self.get_distance(prod[3], prod[4]), 2), current_res/50000.0))
 		for coord, rent, ships in sorted(rentability, key=lambda y: y[1], reverse = True):
 			print coord, rent, ships;
-				
 
 def parse_report(report):
 	s = re.search('Resources at .* \[(.*)\] \(.*\) on (.*)', report[0])
@@ -70,22 +78,34 @@ def parse_report(report):
 	
 bot = ogameDB()
 
-def calculate_production(bot, check = False):
-	bot.cursor.execute('select planet_id from planet;');
+def calculate_production(bot, coord_mask = None, check = False):
+	if coord_mask:
+		bot.cursor.execute("select planet_id from planet where coord like '{}';".format(coord_mask));
+	else:
+		bot.cursor.execute('select planet_id from planet;');
 	planet_list = [ id for id, in bot.cursor.fetchall() ]
+	print 'Calculating production for {} planets'.format(len(planet_list))
 	for id in planet_list:
-		bot.cursor.execute('select date, metal, crystal, deuterium from report where planet_id={} order by date desc;'.format(id))
+		if bot.cursor.execute('select date, metal, crystal, deuterium from report where planet_id={} order by date desc;'.format(id)) < 2:
+			print 'Need another report for planet {} to calculate production.'.format(id)
+			continue
 		x = bot.cursor.fetchall()
+		
 		dt = (x[0][0] - x[1][0]).total_seconds() / 3600.0
-		r = [(x[0][1] - x[1][1])/dt, (x[0][2] - x[1][2])/dt, (x[0][3] - x[1][3])/dt]
+		r = [int(round(i)) for i in [(x[0][1] - x[1][1])/dt, (x[0][2] - x[1][2])/dt, (x[0][3] - x[1][3])/dt]]
 		# check delta production, if it's too big raise an error 
 		if check:
 			bot.cursor.execute('select m_prod, c_prod, d_prod from planet where planet_id = {};'.format(id))
 			r0 = [i for i in bot.cursor.fetchone()]
 			res = {0: 'metal', 1: 'crystal', 2: 'deuterium'}
 			for i in range(3):
+				if r[i] < 0:
+					print 'Invalid production for planet {}'.format(id)
+					print 'Leaving original as is'
+					r = r0
+					break
 				if abs(r0[i] - r[i]) / float(max(r0[i], r[i])) > 1.0/100.0:
-					print 'Warning! Too big a difference in production for resourse {}'.format(res[i])
+					print 'Warning! Too big a difference in production for resourse {} on planet {}'.format(res[i], id)
 					print 'Original was {}, new production is {}. Leaving original as is.'.format(r0[i], r[i])
 					r[i] = r0[i]
 		bot.cursor.execute('update planet set m_prod={}, c_prod={}, d_prod={} where planet_id = {};'.format(r[0], r[1], r[2], id))
@@ -99,13 +119,15 @@ while True:
 		c, d, m, cr, de = parse_report(report)
 		bot.addReport(c, d, m, cr, de)
 	
-	elif re.match('(?i)cprod', command):
-		calculate_production(bot, re.match('cprod -c', command) != None)
+	elif re.match('cprod', command):
+		coord = re.match('cprod (-c ?)?(.*)', command)
+		if (coord): coord = coord.group(2)
+		calculate_production(bot, coord, re.search('-c', command) != None)
 		
-	elif re.match('(?i)rent', command):
+	elif re.match('rent', command):
 		bot.getRentability()
 		
-	elif re.match('(?i)(quit|q)\Z', command):
+	elif re.match('(quit|q)\Z', command):
 		print "Exiting..."
 		break
 	
